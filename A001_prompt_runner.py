d83b554540d96fa524c975c60bd60985b6cc50cd
admin <root@localhost>
2023-08-15 09:07:35 +0000

    ddd

--- A001_prompt_runner.py
+++ A001_prompt_runner.py
@@ -174,6 +174,9 @@ def build_json_from_the_response(action=None, success=None, container=None, resu
     # Write your custom code here...
     
     phantom.debug(check_the_status_of_the_prompt_result_item_0[0][0])
+    response = check_the_status_of_the_prompt_result_item_0[0]
+    
+    response_data = response[0]["data"][0]["response"]
 
     ################################################################################
     ## Custom Code End
